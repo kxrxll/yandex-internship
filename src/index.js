a9e7a90fedddc7d31059fbc64e2ffffebfe26365
kxrxll <kovalyovkirill@gmail.com>
2021-08-15 18:16:59 +0300

    some additions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+/* eslint-disable no-new */
 import './css/styles.css';
 import BooleanCells from './js/app';
 import csvToGameData from './js/parser';
 
-// eslint-disable-next-line no-unused-vars
-let booleanCells = new BooleanCells(8, 8, document.querySelector('.main'));
-
+const button = document.querySelector('button');
 const inputElement = document.querySelector('input');
 
 inputElement.addEventListener('change', (e) => {
@@ -15,7 +14,13 @@ inputElement.addEventListener('change', (e) => {
     const text = evt.target.result;
     const [cellsMap, width, height] = csvToGameData(text);
     document.querySelector('.main').innerHTML = '';
-    booleanCells = new BooleanCells(width, height, document.querySelector('.main'), cellsMap);
+    new BooleanCells(width, height, document.querySelector('.main'), cellsMap);
   };
   reader.readAsText(input);
+});
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.main').innerHTML = '';
+  new BooleanCells(8, 8, document.querySelector('.main'));
 });
